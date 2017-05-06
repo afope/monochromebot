3a136689715bc3e30355d577d125235a827b7d90
Bolutife Lawrence <lawrence.mcbhee@gmail.com>
2017-05-06 17:02:24 +0100

    Refactor code.

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
-var config = {
+const config = {
   consumer_key:         'lIFM3WHKWBip89uispWCSppn6',
   consumer_secret:      '5ZCV2DwssTK3tstnj6IgD62coOTDZtv6WqvnqmyikRR0Qus9S9',
   access_token:         '815899964698193920-3BqS3pcIKLbHM0LrDgvHLQoAc2ss8lS',
   access_token_secret:  'xuRutCp9mA2sxbRI3Tm9sPFFbIZQT8o0cS15JXMO8CBQy'
-}
+};
 
 module.exports = config;
