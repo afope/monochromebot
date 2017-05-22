f13b08c7b365e03249f58cd88ddf3ea407f6be1f
“afope” <fope.ojo@gmail.com>
2017-05-22 22:30:04 +0100

    port

--- app/index.js
+++ app/index.js
@@ -1,3 +1,7 @@
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 var fs = require("fs"),
   path = require("path"),
   Twit = require("twit"),
